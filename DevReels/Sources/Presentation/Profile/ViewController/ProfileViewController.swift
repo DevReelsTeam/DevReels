10cd105855cf684ac4c3b28d128be282f84742b1
Peter <ghdtjrgus333@naver.com>
2023-05-14 10:03:59 +0900

    init: 프로필 파일 트리 및 뷰컨트롤러, 뷰모델 파일 생성

---
+++ DevReels/Sources/Presentation/Profile/ViewController/ProfileViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileViewController.swift
+//  DevReels
+//
+//  Created by Sh Hong on 2023/05/14.
+//  Copyright © 2023 DevReels. All rights reserved.
+//
+
+import Foundation
---
+++ DevReels/Sources/Presentation/Profile/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileViewModel.swift
+//  DevReels
+//
+//  Created by Sh Hong on 2023/05/14.
+//  Copyright © 2023 DevReels. All rights reserved.
+//
+
+import Foundation
