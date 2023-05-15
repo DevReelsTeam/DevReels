92d908444ca26164da3aa643708c97c3e5a243b3
Peter <ghdtjrgus333@naver.com>
2023-05-15 12:42:27 +0900

    feat: 대략적인 input output 추가

--- DevReels/Sources/Presentation/Profile/ViewModel/ProfileViewModel.swift
+++ DevReels/Sources/Presentation/Profile/ViewModel/ProfileViewModel.swift
@@ -15,11 +15,24 @@ class ProfileViewModel: ViewModelType {
     var disposeBag: RxSwift.DisposeBag = .init()
     
     struct Input {
-        
+        let editButtonTapped = PublishRelay<Void>()
+        let githubButtonTapped = PublishRelay<Void>()
+        let blogButtonTapped = PublishRelay<Void>()
+        let followerButtonTapped = PublishRelay<Void>()
+        let followingButtonTapped = PublishRelay<Void>()
+        let postTapped = PublishRelay<Post.ID>()
     }
     
     struct Output {
-
+//        let userImage: Driver<UIImage>
+//        let userName: Driver<String>
+//        let userIntroduction: Driver<String>
+//        let userGithubURL: Driver<URL>
+//        let userBlogURL: Driver<URL>
+//        let postCount: Driver<Count>
+//        let followerCount: Driver<Count>
+//        let followingCount: Driver<Count>
+//        let posts: Driver<[Post]>
     }
     
     func transform(input: Input) -> Output {
