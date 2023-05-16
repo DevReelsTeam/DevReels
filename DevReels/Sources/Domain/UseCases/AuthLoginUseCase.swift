7e93f35cfb035b247df05b5e37b27b1de4c3a225
제리 <gwd0311@naver.com>
2023-05-16 15:05:42 +0900

    chore: 폴더구조 변경 및 example 파일 생성

---
+++ DevReels/Sources/Domain/UseCases/AuthLoginUseCase.swift
@@ -0,0 +1,20 @@
+//
+//  AuthLoginUseCase.swift
+//  DevReels
+//
+//  Created by hanjongwoo on 2023/05/16.
+//  Copyright © 2023 DevReels. All rights reserved.
+//
+
+import RxSwift
+
+// ex
+//struct AutoLoginUseCase: AutoLoginUseCaseProtocol {
+//
+//    var tokenRepository: TokenRepositoryProtocol?
+//    private let disposeBag = DisposeBag()
+//
+//    func load() -> Observable<Authorization> {
+//        return tokenRepository?.load() ?? .empty()
+//    }
+//}
