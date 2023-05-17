a480eed0391a8b0ea02700c035eee47d2f05fcb7
제리 <gwd0311@naver.com>
2023-05-17 16:58:15 +0900

    feat: DTO 기본구조 생성

---
+++ DevReels/Sources/Data/DataMapping/CommentDTO.swift
@@ -0,0 +1,15 @@
+//
+//  CommentDTO.swift
+//  DevReels
+//
+//  Created by hanjongwoo on 2023/05/17.
+//  Copyright © 2023 DevReels. All rights reserved.
+//
+
+import Foundation
+
+struct CommentDTO {
+    private let id: StringValue
+    private let comment: StringValue
+//    private let timestamp = Timestamp
+}
